0c2f6d61609b3080d98220ff765076751a962a83
Dylan Matthews <dylnmatthews@gmail.com>
2022-01-10 17:37:44 +0200

    setup files

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 import setuptools
+from os import path
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+long_description = ''
+if path.exists('README.md'):
+    with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
 
 setuptools.setup(
     name='treblle',
-    version='0.003',
-    install_requires=['requests']
+    version='0.007',
+    install_requires=['requests'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
