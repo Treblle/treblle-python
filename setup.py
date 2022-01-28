344ee98a7a2439b5b43c37124d3f0da46fa1da3e
Dylan Matthews <dylnmatthews@gmail.com>
2022-01-28 08:58:04 +0200

    version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if path.exists('README.md'):
 
 setuptools.setup(
     name='treblle',
-    version='0.010',
+    version='0.011',
     install_requires=['requests'],
     long_description=long_description,
     long_description_content_type="text/markdown",
